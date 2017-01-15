0e0f0b278989777a7334f20a9a3b5984d7dd6a7c
Megan Niu <megan.niu>
2017-01-15 01:19:23 -0500

    Animation works but the change in path when the organism hits a boundary is VERY buggy. TODO: - refine trig operations to properly find the angle of the new path when an org hits a wall - move all code that calculates path to organism class

--- src/DrawArea.java
+++ src/DrawArea.java
@@ -1,46 +1,168 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.Color;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
-
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-
 import com.sun.javafx.geom.Rectangle;
+import javax.swing.Timer;
 
 public class DrawArea extends JPanel{
 	BufferedImage img = null;
+	
+	static Timer timer;
+	
+	Double angle;
+	
+	int posX;
+	int posY;
+	
+	int width;
+	int height;
+	
+	
 	public DrawArea (int width, int height, BufferedImage img){
+		this.width = width;
+		this.height = height;
+		
 		JPanel drawArea= new JPanel();
 		
 		this.setVisible(true);
 		this.setOpaque(true);
-        this.setPreferredSize (new Dimension (width, height)); // size
+        this.setPreferredSize (new Dimension (this.width, this.height)); // size
         //this.setBorder(BorderFactory.createEmptyBorder(10,10,10,10)); 
+
         this.img = img;
+        
+        angle = 45.0;
+    	posX = 100;
+    	posY = 100;
+    	
+        timer = new Timer(50, new ActionListener(){
+    		public void actionPerformed(ActionEvent ev){
+    			
+    			System.out.println("X:" + posX + " Y: " + posY + " Angle:" + angle);
+    			
+    			int[] nextPos = nextPos(posX, posY, angle);
+    			
+    			if(nextPos[0] >= width){
+    				System.out.println("border encountered X");
+    				//nextPos = nextPos(posX, posY, 270 - angle);
+    				//angle = 270 - angle;
+    				if(angle >= 270 && angle <= 360){
+    					nextPos = nextPos(posX, posY, 540 - angle);
+        				angle = 540 - angle;
+    				}
+    				else if(angle <= 90 && angle >= 0){
+    					nextPos = nextPos(posX, posY, 360 - angle);
+    					angle = 360 - angle;
+    				}
+    			}
+    			if(nextPos[0] <= 0){
+    				System.out.println("border encountered X");
+    				if(angle >= 180 && angle <= 270){
+    					nextPos = nextPos(posX, posY, 540 - angle);
+        				angle = 540 - angle;
+    				}
+    				else if(angle < 180 && angle >=90){
+    					nextPos = nextPos(posX, posY, 360 - angle);
+    					angle = 360 - angle;
+    				}
+    			}
+    			
+    			if(nextPos[1] >= height){
+    				System.out.println("border encountered Y, rejX:" + nextPos[0] + " rejY:" + nextPos[1]);
+    				//if(angle >= 0 && angle <= 90){
+    					nextPos = nextPos(posX, posY, 360 - angle);
+        				angle = 360 - angle;
+        				
+        				nextPos[1] = posY + (nextPos[1] - posY) * -1;//disregarding cast rule in this case messes up the path of org Restore the cast rule
+    				//}
+
+    			}
+    			else if(nextPos[1] <= 0){
+    				System.out.println("border encountered Y, rejX:" + nextPos[0] + " rejY:" + nextPos[1]);
+    				nextPos = nextPos(posX, posY, 360 - angle);
+    				angle = 360 - angle;
+    			}
+    			posX = nextPos[0];
+    			posY = nextPos[1];
+    			//System.out.println("nextPosXAL:" + nextPos[0] + " nextPosYAL:" + nextPos[1]);
+    			repaint();
+    		}
+    	});
+        
+        //System.out.println("Width: " + drawArea.getWidth() + " Height: " + drawArea.getHeight());
+        //System.out.println("Width: " + this.width + " Height: " + this.height);
     }
 
     public void paintComponent (Graphics g){
     	super.paintComponent(g);
     	
+    	/**
+    	 * Draws and paints background
+    	 */
     	g.setColor(Color.WHITE);
-    	g.drawRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
-    	g.fillRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
+    	g.drawRect(0, 0, width - 1, height - 1);
+    	g.fillRect(0, 0, width - 1, height - 1);
     	
-    	
+    	/**
+    	 * Rotates image of organism. Only works if image is a SQUARE. Otherwise, image will be cropped when rotated
+    	 */
     	double locX = img.getWidth()/2;
     	double locY = img.getHeight()/2;
-    	AffineTransform tx = AffineTransform.getRotateInstance(Math.toRadians(180), locX, locY);
+    	AffineTransform tx = AffineTransform.getRotateInstance(Math.toRadians(360 - angle), locX, locY);
     	AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-
+    	g.drawImage(op.filter(img, null), posX, posY, null);
     	
-    	g.drawImage(op.filter(img, null), 200, 200, null);
+    	
+    	
+    }
+    
+    public int[] nextPos(int pastX, int pastY, double angle){
+    	/**
+    	 * using tan(x) = rise/run
+    	 * assuming run is always 2 (i.e. moving 2 pixels horizontally each time), rise (change in vertical movement) can 
+    	 * be expressed as rise = 2tan(x)
+    	 * where x is the angle of movement
+    	 */
+    	
+    	double nextYDouble = Math.tan(Math.toRadians(angle)) * 2;//expression of rise in terms of angle
+    	int nextY = (int) nextYDouble;
+    	
+    	if(angle > 180 && angle < 270){//disregard cast rule
+    		nextY = nextY * (-1);
+    	}
 
-    	//repaint();
+    	if(angle == 270.0){
+    		nextY = -2;//if angle is 270 move vertically down by 2
+    	}
+    	else if(angle == 90.0){ //for tan(x), 90 and 270 are asymptotes
+    		nextY = 2;//if angle is 90, move vertically up 2
+    	}
+    	
+    	
+    	int[] nextPos = new int[2]; //{nextX, nextY}  
+    	
+    	if(angle == 90.0 || angle == 270.0){
+    		nextPos[0] = pastX;//traveling vertically, no change in x
+    	}
+    	else if((angle >= 0 && angle < 90.0) || (angle >270.0 && angle <= 360.0)){
+        	nextPos[0] = pastX + 2; //moving 2 pixels horizontally to the right
+    	}
+    	else{
+    		nextPos[0] = pastX - 2; //moving 2 pixels horizontally to the left
+    	}
+    	
+    	nextPos[1] = pastY - nextY;//moving the organism vertically (direction dep on earlier calcs)
+    	
+    	return nextPos;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main extends JFrame{
 	static BufferedImage img = null;
+	
+	boolean startSim; //true to start sim, false to pause sim
 	
 	public Main(){
 		JFrame main = new JFrame();
@@ -16,14 +21,43 @@ public class Main extends JFrame{
 		main.setVisible(true);
 		main.setResizable(false);
 		
-		DrawArea drawArea = new DrawArea(500, 300, img);
 		main.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
+		
+		JButton go = new JButton("Go");
+		go.addActionListener(new BtnListener());
+		startSim = false;
+		
+		System.out.println("here2");
+		
 		gbc.gridx = 0;
+		gbc.gridy = 0;
+		main.add(go, gbc);
+		
+		DrawArea drawArea = new DrawArea(500, 300, img);
+		
+		gbc.gridx = 1;
 		gbc.gridy = 0;
 		//gbc.weighty = 0.8;
 		
 		main.add(drawArea, gbc);
+		
+	}
+	
+	public class BtnListener implements ActionListener{
+		BtnListener(){
+		}
+		
+		public void actionPerformed(ActionEvent e){
+			System.out.println("here1");
+			if(startSim == false){
+				DrawArea.timer.start();
+			}
+			else{
+				DrawArea.timer.stop();
+			}
+			startSim = !startSim;
+		}
 		
 	}
 	
@@ -37,6 +71,6 @@ public class Main extends JFrame{
 			System.out.println("organism.png not found.");
 		}
 		
-		Main simulation = new Main();
+		Main Simulation = new Main();
 	}
 }
