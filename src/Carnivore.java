571d25ae5a5102315d088e8cad221a40cb224eb6
MacDoc77 <vwvincentwang3@gmail.com>
2017-01-19 13:44:46 -0500

    Update Carnivore.java

--- src/Carnivore.java
+++ src/Carnivore.java
@@ -68,6 +68,8 @@ public class Carnivore extends Organism {
 			Point hPoint = DrawArea.herbivores.get(i).getPoint();
 			double distance = Math.hypot(pos.x - hPoint.x, pos.y - hPoint.y);
 			if (distance <= 10) {
+				energy += herbivores.get(i).getEnergy() / 5;
+				eggCycles++;
 				DrawArea.herbivores.remove(i);
 				i--;
 			}
