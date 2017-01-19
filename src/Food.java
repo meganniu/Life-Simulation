a7a7a5ed530566b659aa2da6d2872ed036a283aa
MacDoc77 <vwvincentwang3@gmail.com>
2017-01-19 13:22:57 -0500

    Update Food.java

--- src/Food.java
+++ src/Food.java
@@ -1,4 +1,20 @@
+public class Food
+{
+    // instance variables - replace the example below with your own
+    private Point pos;
+    private Hitbox box;
+    private int nutrition;
+    public Food(int nutritrion, Point pos)
+    {
+        this.pos = pos;
+        box = new Hitbox(pos.x - 1, pos.y - 1, 3, 3);
+    }
 
-public class Food {
-
+    public Point getPos(){
+        return pos;
+    }
+    
+    public void remove (int i){
+        drawArea.food.remove(i);
+    }
 }
