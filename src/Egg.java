d0291db9b599015e64dd169f0fe7f997800b7933
MacDoc77 <vwvincentwang3@gmail.com>
2017-01-19 13:59:39 -0500

    Update Egg.java

--- src/Egg.java
+++ src/Egg.java
@@ -3,26 +3,28 @@ public class Egg
     private double hatchTime;
     private Point pos;
     private boolean parent;
-
     /**
      * Constructor for objects of class Egg
      */
-    public Egg(double hatchTime, Point pos, boolean parent)
+    public Egg(Point pos, int speed, int detectRadius, int gen, int hatchTime, boolean parent)
     {
-        this.hatchTime = hatchTime;
         this.pos = pos;
+        this.speed = speed;
+        this.detectRadius = detectRadius;
+        this.gen = gen;
+        this.hatchTime = hatchTime;
         this.parent = parent;
     }
     
     public void hatch(int i){
         if (parent){
             drawArea.eggs.remove(i);
-            Carnivore baby = new Carnivore(pos, Math.floor(Math.random() * 359) + 0 ,  10, 80, 0, 0);
+            //Carnivore baby = new Carnivore(pos, Math.floor(Math.random() * 359) + 0 ,  10, 80, 0, 0);
             drawArea.carnivores.add(baby);
         }
         else{
             drawArea.eggs.remove(i);
-            Herbivore baby = new Herbivore(pos, Math.floor(Math.random() * 359) + 0 ,  10, 80, 0, 0);
+            //Herbivore baby = new Herbivore(pos, Math.floor(Math.random() * 359) + 0 ,  10, 80, 0, 0);
             drawArea.herbivores.add(baby);
         }
     }
