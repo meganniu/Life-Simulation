c050009c77bb4a7fa5fe089b0cca273ea18870f1
MacDoc77 <vwvincentwang3@gmail.com>
2017-01-19 20:19:29 -0500

    Add files via upload

---
+++ src/Hitbox.java
@@ -0,0 +1,12 @@
+public class Hitbox extends Rectangle
+{
+    private int xpos, ypos, l, w
+    public Hitbox(int xpos, int ypos, int l, int w)
+    {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.l = l;
+        this.w = w;
+    }
+
+}
